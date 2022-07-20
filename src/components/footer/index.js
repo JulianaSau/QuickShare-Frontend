e1245641bc6e74de984c108048d49ce37de7ddf1
Sau Juliana <julienyange@gmail.com>
2022-07-20 08:55:03 +0300

    new changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import "./style.css";
 import Logo from "../static/Logo.svg";
 import { motion } from "framer-motion";
-import Avatar from "../avatarMain";
 
-const Footer = ({ index }) => {
+const Footer = () => {
   return (
     <div>
       <motion.div
@@ -13,18 +12,12 @@ const Footer = ({ index }) => {
         animate={{ y: 0 }}
       >
         {/* <img className = "logo" src = {Logo} alt = "Vegh Logo"/> */}
-        <p className="logo">QuickShare</p>
         <div className="linkContainer">
           {/* <p className = "about"><a href = "https://veghfile.github.io">About</a><span> |</span></p>
                 <p className = "tos"><a href = "https://veghfile.github.io/tos">Terms of Service</a><span> |</span></p>
                 <p className = "privacy"><a href = "https://veghfile.github.io/privacy">Privacy</a></p> */}
-          <p className="copyright">2022 All Right Reserved</p>
+          <p className="privacy">QuickShare</p>
         </div>
-        {/* <div className="userInfo">
-          <Avatar index={index}>
-            <p>You</p>
-          </Avatar>
-        </div> */}
       </motion.div>
     </div>
   );
--- src/modules/IconButton/index.js
+++ src/modules/IconButton/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import "./style.css";
-import { motion } from "framer-motion";
-const IconButton = ({ svg, children, ...other }) => {
-  const transition = { duration: 0.1 };
-  return (
-    <div>
-      <motion.button
-        {...other}
-        className="button-primary"
-        type="button"
-        transition={transition}
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        {children ? children : ""}
-        {svg ? <img src={svg} alt="icon" /> : null}
-      </motion.button>
-    </div>
-  );
-};
+import React from 'react'
+import './style.css'
+import {motion} from 'framer-motion'
+const IconButton = ({svg,children,...other}) => {
+    const transition  = { duration : .1 };
+    return (
+        <div>
+            <motion.button {...other} className="button-primary" type="button"  transition = {transition} whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}>
+                {children?children:"Share Link"}
+                <img src = {svg}/>
+            </motion.button>
+        </div>
+    )
+}
 
-export default IconButton;
+export default IconButton
